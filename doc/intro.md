4c31ae7364e018d32cdf444a265a29bd6f51fcfe
Michiel van Oosten <mail@axion5.net>
2014-12-11 00:51:04 +0100

    first-commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nonograms
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/nonograms/core_test.clj
@@ -0,0 +1,7 @@
+(ns nonograms.core-test
+  (:require [clojure.test :refer :all]
+            [nonograms.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
